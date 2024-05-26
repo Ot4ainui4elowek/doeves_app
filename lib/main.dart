a73957182b193368faed02342554a51c76bd8067
Bogdan Onischenko <bogdan232137@list.ru>
2024-05-26 18:40:49 +0300

    edit some files after adding DI container

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:doeves_app/core/domain/container/app_container.dart';
 import 'package:doeves_app/feauture/doeves_app/presentation/doeves_app_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const DoevesApp());
+  runApp(DoevesApp(
+    appContainer: AppContainer.init(),
+  ));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:doeves_app/core/domain/container/app_container.dart';
 import 'package:doeves_app/feauture/doeves_app/presentation/doeves_app_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,7 +13,9 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const DoevesApp());
+    await tester.pumpWidget(DoevesApp(
+      appContainer: AppContainer.init(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
