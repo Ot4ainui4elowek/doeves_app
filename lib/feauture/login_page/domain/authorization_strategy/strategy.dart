d89f72adad02cbe1459125eaefdf12ce8b989c3c
Bogdan Onischenko <bogdan232137@list.ru>
2024-05-31 22:08:58 +0300

    edit repository signUp signature & add new auth strategy

--- lib/feauture/login_page/domain/authorization_strategy/strategy.dart
+++ lib/feauture/login_page/domain/authorization_strategy/strategy.dart
@@ -1,12 +1,15 @@
 import 'package:doeves_app/core/domain/use_case_result/use_case_result.dart';
 import 'package:doeves_app/feauture/login_page/data/model/sign_in_response_model.dart';
+import 'package:doeves_app/feauture/login_page/data/model/sign_up_response_model.dart';
 import 'package:doeves_app/feauture/login_page/domain/repository/authorization_repository.dart';
 
-abstract class AuthorizationStrategy {
+abstract class SignInStrategy {
   Future<UseCaseResult<SignInResponseModel>> call();
 }
 
-abstract class SignInStrategy implements AuthorizationStrategy {}
+abstract class SignUpStrategy {
+  Future<UseCaseResult<SignUpResponseModel>> call();
+}
 
 class SignInUsingEmailStrategy implements SignInStrategy {
   SignInUsingEmailStrategy({
@@ -21,5 +24,28 @@ class SignInUsingEmailStrategy implements SignInStrategy {
   Future<UseCaseResult<SignInResponseModel>> call() async {
     return await authorizationRepository.signIn(
         email: email, password: password);
+  }
+}
+
+class SignUpUsingEmailStrategy implements SignUpStrategy {
+  SignUpUsingEmailStrategy(
+    this.authorizationRepository, {
+    required this.fullName,
+    required this.email,
+    required this.password,
+    required this.retypePassword,
+  });
+  final String email;
+  final String password;
+  final String retypePassword;
+  final String fullName;
+  final AuthorizationRepository authorizationRepository;
+  @override
+  Future<UseCaseResult<SignUpResponseModel>> call() async {
+    return await authorizationRepository.signUp(
+        fullName: fullName,
+        email: email,
+        password: password,
+        retypePassword: retypePassword);
   }
 }
--- lib/feauture/login_page/domain/repository/authorization_repository.dart
+++ lib/feauture/login_page/domain/repository/authorization_repository.dart
@@ -1,6 +1,6 @@
 import 'package:doeves_app/core/domain/use_case_result/use_case_result.dart';
 import 'package:doeves_app/feauture/login_page/data/model/sign_in_response_model.dart';
-import 'package:doeves_app/feauture/login_page/domain/entity/authorization_credentrials.dart';
+import 'package:doeves_app/feauture/login_page/data/model/sign_up_response_model.dart';
 
 abstract class AuthorizationRepository {
   Future<UseCaseResult<SignInResponseModel>> signIn({
@@ -8,7 +8,7 @@ abstract class AuthorizationRepository {
     required String password,
   });
 
-  Future<UseCaseResult<AuthorizationCredentials>> signUp({
+  Future<UseCaseResult<SignUpResponseModel>> signUp({
     required String fullName,
     required String email,
     required String password,
